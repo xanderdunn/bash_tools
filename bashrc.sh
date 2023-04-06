4f660a85267c8dda675d2524585750e60bfb9c84
Xander Dunn <xander@xander.ai>
2023-04-06 19:34:19 -0400

    Add GPG pub key

--- bashrc.sh
+++ bashrc.sh
@@ -41,6 +41,7 @@ alias untar='tar -xvzf'
 alias password="pwgen -s -y 30 1"
 alias vimrc="nvim ~/bash_tools/vimrc"
 alias devenv='export CUDA_VISIBLE_DEVICES=""; wandb off'
+alias gpgtest='echo "test" | gpg --clearsign'
 # alias gscopypreds="gsutil -m cp predictions/* gs://praxis-data-plane/xander/"
 # }}} 
 
